04b2eb4730de9b98de7464fa42686c8f3999195c
Kawamura Takaki <surrealist1927@gmail.com>
2020-04-03 09:24:07 +0900

    No.4 fin borderline set

--- app/src/main/java/com/tutorial/catchtheball/MainActivity.java
+++ app/src/main/java/com/tutorial/catchtheball/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
     private ImageView orange;
     private ImageView pink;
     private ImageView black;
+
+    //サイズ
+    private int frameHeight;
+    private int boxSize;
 
     //位置
     private float boxY;
@@ -60,20 +65,32 @@ public class MainActivity extends AppCompatActivity {
         }else {
             boxY += 20;
         }
+
+        if(boxY < 0) boxY = 0;
+
+        //frameの高さからboxの高さを引いた値
+        //スマホでは左上の座標が0となる。
+        //相対距離を使用しているため端末サイズに基づくバグが消失する。
+        if(boxY > frameHeight - boxSize) boxY = frameHeight - boxSize;
+
         box.setY(boxY);
     }
 
     //code→overridemethodから検索可能
     @Override //上書き処理
     public boolean onTouchEvent(MotionEvent event) {
-        if (start_flg){
-            if (event.getAction() == MotionEvent.ACTION_DOWN){
-                action_flg = true;
-            }else if (event.getAction() == MotionEvent.ACTION_UP){
-                action_flg = false;
-            }
-        }else{
+        if (start_flg == false){
             start_flg = true;
+
+            //onCreateではビューの描画が完了していないため、確実に完了しているonTouchEventでサイズを取得
+            //onCreateが終了した時点でビューの描画は完了する。
+            //FrameLayoutは要素をはめるための枠組みなので、初期値としてHeightなどは存在していない。
+            FrameLayout frame = findViewById(R.id.frame);
+            frameHeight = frame.getHeight();
+
+            boxY = box.getY();
+            boxSize = box.getHeight();
+
             //Invisibleは非表示にする。GONEは完全に消すという違い。
             startLabel.setVisibility(View.GONE);
 
@@ -88,6 +105,12 @@ public class MainActivity extends AppCompatActivity {
                     });
                 }
             }, 0, 20);
+        }else{
+            if (event.getAction() == MotionEvent.ACTION_DOWN){
+                action_flg = true;
+            }else if (event.getAction() == MotionEvent.ACTION_UP){
+                action_flg = false;
+            }
         }
         return true;
     }
